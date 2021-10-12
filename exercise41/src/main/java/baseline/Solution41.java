e60f1b3f518bd3f003dd45bf2afbf48ac4495b73
Miguel <miguelangel53013@gmail.com>
2021-10-12 02:42:43 -0400

    committing initial code, no test cases or UML present

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -10,33 +10,68 @@ import java.io.*;
 public class Solution41 {
     public static void main(String[] args) {
         // Create a Solution41 object
+        Solution41 app = new Solution41();
         // Create an arraylist of strings and call scanNames()
+        List<String> nameList = app.scanNames();
         // Create a stringbuilder and call createNameListString() with the previous arraylist
+        StringBuilder nameListString = app.createNameListString(nameList);
         // Call uploadListToFile() with the previously created stringbuilder
+        app.uploadListToFile(nameListString);
     }
 
-    public ArrayList<String> scanNames() {
+    public List<String> scanNames() {
         // Create an arraylist of strings to hold each name.
+
+
+        ArrayList<String> nameList = new ArrayList<>();
         // Create a file object and path the object to exercise41_input.txt in the data folder
-        // Loop through the file until the EOF is reached
-            // Scan each line into the arraylist as its own string
+        // Use a try-with-resources to ensure that all resources are closed
+        try (BufferedReader br = new BufferedReader(new FileReader("data/exercise41_input.txt"))) {
+            String currentName;
+            // Loop through the file until the EOF is reached (Or null actually)
+            while ((currentName = br.readLine()) != null) {
+                // Scan each line into the arraylist as its own string
+                nameList.add(currentName);
+            }
+        } catch (IOException e) {
+            System.out.println("Error scanning in file.");
+        }
         // Return the list
+        return nameList;
     }
 
-    public StringBuilder createNameListString(ArrayList<String> nameList) {
+    public StringBuilder createNameListString(List<String> nameList) {
         // Create a stringbuilder object
+        StringBuilder nameListString = new StringBuilder();
         // call sortByLastName on nameList
+        sortByLastName(nameList);
         // Create the top part of the string with "Total of nameList.length Names" followed by a line
+        nameListString.append("Total of ").append(nameList.size()).append(" names\n");
+        nameListString.append("-----------------\n");
         // Loop through the arraylist adding each object to the string
+        for (String s : nameList) {
+            nameListString.append(s).append("\n");
+        }
+        // Return the completed string
+        return nameListString;
     }
 
     public void uploadListToFile(StringBuilder nameListString) {
+        // Create a file object to store the file/path to the output or create if necessary
+        File file = new File("data/exercise41_output.txt");
         // Create a printwriter object and path it to exercise41_output.txt
-        // Print the string into the file
+        // Use a try-with-resources to ensure that all resources are closed
+        try (PrintWriter writer = new PrintWriter(file)) {
+            // Print the string into the file
+            writer.write(String.valueOf(nameListString));
+        } catch (IOException e) {
+            System.out.println("Error writing to a file.");
+        }
     }
 
-    private ArrayList<String> sortByLastName(ArrayList<String> nameList) {
-        // Return the list having called the sort command on it
+    private void sortByLastName(List<String> nameList) {
+        // Alter the list to be sorted
+        Collections.sort(nameList);
     }
 
 }
